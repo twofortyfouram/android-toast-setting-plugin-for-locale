3f0f231d4d76fd54289ba1d0c58fda1f752f0916
Carter Jernigan <git@carterjernigan.com>
2013-12-24 14:50:37 -0500

    Initial port

---
+++ README.md
@@ -0,0 +1,17 @@
+[![Build Status](https://travis-ci.org/twofortyfouram/android-toast-setting-plugin-for-locale.png?branch=master)](https://travis-ci.org/twofortyfouram/android-toast-setting-plugin-for-locale)
+
+# Overview
+[Locale](https://play.google.com/store/apps/details?id=com.twofortyfouram.locale) allows developers to create plug-in conditions and settings.  The android-toast-setting-plugin-for-locale implements an example plug-in setting.  This project is the final layer of the [Locale Developer Platform documentation](http://www.twofortyfouram.com/developer).
+
+Although there are multiple ways to approach building a plug-in, we recommend forking this project as the starting point.
+
+
+# Compatibility
+The application is compatible and optimized for Android API Level 8 and above.
+
+
+## Forking this Project
+The following steps are necessary to fork this project
+
+1. Rename the package name in AndroidManifest.xml
+1. Rename the package name in proguard-project.txt
---
+++ toast-plugin-setting-app/src/main/java/com/twofortyfouram/locale/example/setting/toast/ui/activity/EditActivity.java
@@ -0,0 +1,75 @@
+/*
+ * android-toast-setting-plugin-for-locale <https://github.com/twofortyfouram/android-toast-setting-plugin-for-locale>
+ * Copyright 2014 two forty four a.m. LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.twofortyfouram.locale.example.setting.toast.ui.activity;
+
+import com.twofortyfouram.locale.example.setting.toast.R;
+import com.twofortyfouram.locale.example.setting.toast.bundle.PluginBundleValues;
+import com.twofortyfouram.locale.sdk.client.ui.activity.AbstractLocalePluginActivity;
+
+import net.jcip.annotations.NotThreadSafe;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.widget.EditText;
+
+@NotThreadSafe
+public final class EditActivity extends AbstractLocalePluginActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public void onPostCreateWithPreviousResult(@NonNull final Bundle previousBundle,
+            @NonNull final String previousBlurb) {
+        final String message = PluginBundleValues.getMessage(previousBundle);
+        ((EditText) findViewById(android.R.id.text1)).setText(message);
+    }
+
+    @Override
+    public boolean isBundleValid(@NonNull final Bundle bundle) {
+        return PluginBundleValues.isBundleValid(bundle);
+    }
+
+    @Override
+    public Bundle getResultBundle() {
+        Bundle result = null;
+
+        final String message = ((EditText) findViewById(android.R.id.text1)).getText().toString();
+        if (message.length() > 0) {
+            result = PluginBundleValues.generateBundle(getApplicationContext(), message);
+        }
+
+        return result;
+    }
+
+    @Override
+    public String getResultBlurb(@NonNull final Bundle bundle) {
+        final String message = PluginBundleValues.getMessage(bundle);
+
+        final int maxBlurbLength = getResources().getInteger(
+                R.integer.com_twofortyfouram_locale_sdk_client_maximum_blurb_length);
+
+        if (message.length() > maxBlurbLength) {
+            return message.substring(0, maxBlurbLength);
+        }
+
+        return message;
+    }
+}
