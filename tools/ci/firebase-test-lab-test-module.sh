7ee321bc8040689175edf8c3097bbdda3fa2f8a2
Carter Jernigan <git@carterjernigan.com>
2017-04-03 14:53:38 -0700

    Update Firebase Test Lab syntax

---
+++ tools/ci/firebase-test-lab-test-module.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Executes tests on a specified module and extracts the results into the 
+# standard Android test directories.  This uses the default Firebase Test Lab
+# bucket with a custom results directory, in order to be able to copy the
+# results back programmatically.
+#
+# This script only runs a test on a single device configuration at a time, so it
+# does not support matrices.  In order to create a matrix, invoke this script
+# multiple times with additional arguments.
+#
+# When files are copied off of gcloud, they are given unique names to ensure
+# that multiple invocations of this script will not cause naming collisions.
+# 
+# This script expects the following parameters in order:
+
+# 1. module tested
+# 2. Default Firebase test lab bucket.  You'll need to invoke the gcloud command manually and find the bucket name to pass for this parameter.
+# 3. Firebase Test Lab device as a quoted string.  E.g. "model=NexusLowRes,version=25,orientation=portrait"
+# 4. Additional Firebase Test Lab arguments, as a quoted string.
+
+# Example usage:
+# ./tools/ci/firebase-test-lab-test-module.sh spackleLib test-lab-beep-boop "model=NexusLowRes,version=25,orientation=portrait"
+
+set -e
+
+module="$1"
+default_bucket="$2"
+device="$3"
+gcloud_args="$4"
+
+uuid=`uuidgen`
+
+gcloud firebase test android run --results-dir=${uuid} tools/gcloud.yml:${module} --device ${device} ${gcloud_args}
+
+# Copy test results.
+echo "Fetching test results"
+mkdir -p ${module}/build/outputs/androidTest-results/connected/
+gsutil -m cp -r "gs://${default_bucket}/${uuid}/**test_result_*.xml" "${module}/build/outputs/androidTest-results/connected/${uuid}-test-result.xml"
+
+# Copy coverage.
+echo "Fetching coverage"
+mkdir -p ${module}/build/outputs/code-coverage/connected/
+gsutil -m cp -r "gs://${default_bucket}/${uuid}/**/artifacts/coverage.ec" "${module}/build/outputs/code-coverage/connected/${uuid}-coverage.ec"
