a5845692bf11e7f30fe8b4b34c378bba1b5ab7c6
Carter Jernigan <git@carterjernigan.com>
2021-08-02 14:44:26 -0400

    Remove authentication for GitHub Package Registry
    https://github.blog/changelog/2021-08-02-github-app-user-to-server-rest-api-requests-now-have-read-access-to-public-resources/

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,15 +17,8 @@ dependencyResolutionManagement {
         google()
         maven {
             // For the Locale Plug-in SDK artifacts
-            // GitHub Package Registry does not allow anonymous access
             val localePluginMavenUrl: String by settings
-            val localePluginMavenUser: String by settings
-            val localePluginMavenPassword: String by settings
             url = java.net.URI(localePluginMavenUrl)
-            credentials  {
-                username = localePluginMavenUser
-                password = localePluginMavenPassword
-            }
         }
     }
 }
