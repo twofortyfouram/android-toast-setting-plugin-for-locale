992e5aeb60aad82d9537a82960ed3901e6552472
Carter Jernigan <git@carterjernigan.com>
2021-08-20 08:07:59 -0400

    Filter unstable dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,3 +24,27 @@ fladle {
     serviceAccountCredentials.set(File(toastPluginFirebaseTestLabServiceAccountKeyPath))
     devices.addAll(mapOf("model" to "NexusLowRes", "version" to "30"))
 }
+
+tasks {
+    withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+        gradleReleaseChannel = "current"
+
+        resolutionStrategy {
+            componentSelection {
+                all {
+                    if (isNonStable(candidate.version) && !isNonStable(currentVersion)) {
+                        reject("Unstable")
+                    }
+                }
+            }
+        }
+    }
+}
+
+val unstableKeywords = listOf("alpha", "beta", "rc", "m", "ea", "build")
+
+fun isNonStable(version: String): Boolean {
+    val versionLowerCase = version.toLowerCase()
+
+    return unstableKeywords.any { versionLowerCase.contains(it) }
+}
